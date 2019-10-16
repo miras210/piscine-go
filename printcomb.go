301786886df16c2e1c48cb6f2d9d3bcab1fa0db1
Miras <sektor.miras@gmail.com>
2019-10-16 11:36:41 +0600

    comb

--- printcomb.go
+++ printcomb.go
@@ -3,18 +3,21 @@ package printcomb
 import "github.com/01-edu/z01"
 
 func PrintComb() {
-	var a int = 48
-	var b int = 49
-	var c int = 50
-	for i:=0; i <= 6; i++ {
-		for j:=i; j <=7; j++ {
-			for k:=j; k <=7; k++ {
-				z01.PrintRune(rune(a+i))
-				z01.PrintRune(rune(b+j))
-				z01.PrintRune(rune(c+k))
-				z01.PrintRune(44)
-				z01.PrintRune(32)
-				
+	var a rune = '0'
+	var b rune = '1'
+	var c rune = '2'
+	for i := a; i <= 54; i++ {
+		for j := b; j <= 56; j++ {
+			for k:= c; k <= 57; k++ {
+				if i < j {
+					if j < k {
+						z01.PrintRune(i)
+						z01.PrintRune(j)
+						z01.PrintRune(k)
+						z01.PrintRune(44)
+						z01.PrintRune(32)
+					}
+				}
 			}
 		}
 	}
