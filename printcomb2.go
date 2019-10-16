673f20023cc286e8b29cac7164040f497c84335a
miras210 <44520791+miras210@users.noreply.github.com>
2019-10-16 19:34:32 +0600

    Update printcomb2.go

--- printcomb2.go
+++ printcomb2.go
@@ -6,14 +6,14 @@ func PrintComb2() {
 	var a1 rune = '0'
 	var a2 rune = '0'
 	var b1 rune = '0'
-	var b2 rune = '1'
+	var b2 rune = '0'
 	for d := a1; d <= 57; d++ {
 		for v := a2; v < 57; v++ {
 			for i := b1; i <= 57; i++ {
 				for j := b2; j <= 57; j++ {
 					if d == '9' && v == '8' && i == '9' && j == '9' {
 						break
-					} else if d < i && j != '0' {
+					} else if d < i && {
 						z01.PrintRune(d)
 						z01.PrintRune(v)
 						z01.PrintRune(32)
@@ -22,7 +22,7 @@ func PrintComb2() {
 						z01.PrintRune(44)
 						z01.PrintRune(32)
 					} else if d == i {
-						if v < j && j != '0' {
+						if v < j && j {
 							z01.PrintRune(d)
 							z01.PrintRune(v)
 							z01.PrintRune(32)
