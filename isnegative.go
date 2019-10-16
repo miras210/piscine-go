2c73d1c911163f4800339d363134e2991ba677c2
Miras <sektor.miras@gmail.com>
2019-10-16 09:42:24 +0600

    wtf

--- isnegative.go
+++ isnegative.go
@@ -4,7 +4,7 @@ import piscine ".."
 import "github.com/01-edu/z01"
 
 func IsNegative(nb int) {
-	if nb > 0 {
+	if nb < 0 {
 		z01.PrintRune('T')
 	} else {
 		z01.PrintRune('F')
@@ -13,5 +13,7 @@ func IsNegative(nb int) {
 
 func main() {
 
-	fmt.IsNegative(1)
+	piscine.IsNegative(1)
+	piscine.IsNegative(0)
+	piscine.IsNegative(-1)
 }
