ecec139f4431091f11acb72b2b5837c2590fb20d
Miras <sektor.miras@gmail.com>
2019-10-16 09:48:59 +0600

    wtf

--- isnegative.go
+++ isnegative.go
@@ -1,19 +1,18 @@
 package main
 
-import piscine ".."
 import "github.com/01-edu/z01"
 
 func IsNegative(nb int) {
 	if nb < 0 {
 		z01.PrintRune('T')
+		z01.PrintRune(10)
 	} else {
 		z01.PrintRune('F')
+		z01.PrintRune(10)
 	}
 }
-
 func main() {
-
-	piscine.IsNegative(1)
-	piscine.IsNegative(0)
-	piscine.IsNegative(-1)
+	IsNegative(1)
+	IsNegative(0)
+	IsNegative(-1)
 }
