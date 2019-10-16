17e424b899ada32556e9fdecf7c139e6ba11a08e
miras210 <44520791+miras210@users.noreply.github.com>
2019-10-16 22:04:53 +0600

    max neg

--- printnbr.go
+++ printnbr.go
@@ -12,37 +12,60 @@ func PrintNbr(n int) {
 	if n == 0 {
 		z01.PrintRune('0')
 	} else if n < 0 {
-		z01.PrintRune('-')
-		n = n * (-1)
-		num = n
-		save = n
-		for n >= 10 {
-			for num >= 10 {
-				num = num / 10
-				count++
-			}
-			save = num
-			for num != 0 {
-				numR = numR + 1
-				num--
-			}
-			for i := a; i <= 127; i++ {
-				if i == numR {
-					z01.PrintRune(i)
-				}
-			}
-			for j := 0; j < count; j++ {
-				ten = ten * 10
-			}
-			count = 0
-			n = n - ten*save
-			if n < ten/10 {
-				z01.PrintRune('0')
-			}
-			save = n
-			ten = 1
+		if n == -9223372036854775808 {
+			z01.PrintRune('-')
+			z01.PrintRune('9')
+			z01.PrintRune('2')
+			z01.PrintRune('2')
+			z01.PrintRune('3')
+			z01.PrintRune('3')
+			z01.PrintRune('7')
+			z01.PrintRune('2')
+			z01.PrintRune('0')
+			z01.PrintRune('3')
+			z01.PrintRune('6')
+			z01.PrintRune('8')
+			z01.PrintRune('5')
+			z01.PrintRune('4')
+			z01.PrintRune('7')
+			z01.PrintRune('7')
+			z01.PrintRune('5')
+			z01.PrintRune('8')
+			z01.PrintRune('0')
+			z01.PrintRune('8')
+		} else {
+			z01.PrintRune('-')
+			n = n * (-1)
 			num = n
-			numR = 48
+			save = n
+			for n >= 10 {
+				for num >= 10 {
+					num = num / 10
+					count++
+				}
+				save = num
+				for num != 0 {
+					numR = numR + 1
+					num--
+				}
+				for i := a; i <= 127; i++ {
+					if i == numR {
+						z01.PrintRune(i)
+					}
+				}
+				for j := 0; j < count; j++ {
+					ten = ten * 10
+				}
+				count = 0
+				n = n - ten*save
+				if n < ten/10 {
+					z01.PrintRune('0')
+				}
+				save = n
+				ten = 1
+				num = n
+				numR = 48
+			}
 		}
 		numR = 48
 		for i := 0; i <= n; i++ {
