6cc61c7b48017dc08f15b5b4f13e30a8e879b1e7
Miras <sektor.miras@gmail.com>
2019-10-17 09:34:43 +0600

    strrev

---
+++ strrev.go
@@ -0,0 +1,13 @@
+package strrev
+
+func StrRev(s string) string {
+	var count int = -1
+	for range s {
+		count++
+	}
+	sA := []byte(s)
+	for i := 0; i <=count; i++ {
+		sA[i] = s[count-i]
+	}
+	return string(sA)
+}
