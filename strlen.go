9fe1d57edb8b0421718f58a53d83b49515c5157f
Miras <sektor.miras@gmail.com>
2019-10-17 09:21:11 +0600

    strlen

--- strlen.go
+++ strlen.go
@@ -2,8 +2,8 @@ package strlen
 
 func StrLen(str string) int {
 	var count int = 0
-	for i, _ := range str {
-		count = i
+	for _, _ = range str {
+		count++
 	}
-	return count + 1
+	return count
 }
