5bc3bc5bf706e965b64824c0e3c104a07b647492
miras210 <44520791+miras210@users.noreply.github.com>
2019-10-17 19:44:55 +0600

    Update basicatoi2.go

--- basicatoi2.go
+++ basicatoi2.go
@@ -3,7 +3,7 @@ package basicatoi2
 func BasicAtoi2(s string) int {
 	var ans int = 0
 	for _, char := range s {
-		if int(char) - 48 >= 0 && int(char) - 48 <= 9 {
+		if int(char)-48 >= 0 && int(char)-48 <= 9 {
 			ans = 10 * ans
 			ans = ans + int(char) - 48
 		} else {
