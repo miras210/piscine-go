1cef247dc74c759832ffc1fd850c602587541cd4
miras210 <44520791+miras210@users.noreply.github.com>
2019-10-17 20:45:03 +0600

    Update atoi.go

--- atoi.go
+++ atoi.go
@@ -2,12 +2,12 @@ package atoi
 
 func Atoi(s string) int {
 	var ans int = 0
-	var sign bool = false;
+	var sign bool = false
 	if s == "" {
 		return 0
 	} else {
 		if s[0] == '-' || s[0] == '+' {
-			sign = true;
+			sign = true
 		}
 		if s[0] == '-' {
 			for _, char := range s {
@@ -15,21 +15,21 @@ func Atoi(s string) int {
 					sign = false
 					continue
 				}
-				if int(char) - 48 >= 0 && int(char) - 48 <= 9 {
+				if int(char)-48 >= 0 && int(char)-48 <= 9 {
 					ans = 10 * ans
 					ans = ans + int(char) - 48
 				} else {
 					return 0
 				}
 			}
-			return (-1)*ans
+			return (-1) * ans
 		} else if s[0] == '+' {
 			for _, char := range s {
 				if sign {
 					sign = false
 					continue
 				}
-				if int(char) - 48 >= 0 && int(char) - 48 <= 9 {
+				if int(char)-48 >= 0 && int(char)-48 <= 9 {
 					ans = 10 * ans
 					ans = ans + int(char) - 48
 				} else {
