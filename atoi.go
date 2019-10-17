4c8c202f15e2605d22193387c8104fa8b42eb384
Miras <sektor.miras@gmail.com>
2019-10-17 20:40:21 +0600

    atoi

--- atoi.go
+++ atoi.go
@@ -3,47 +3,50 @@ package atoi
 func Atoi(s string) int {
 	var ans int = 0
 	var sign bool = false;
-	if s[0] == '-' || s[0] == '+' {
-		sign = true;
-	}
-	if s[0] == '-' {
-		for _, char := range s {
-			if sign {
-				sign = false
-				continue
-			}
-			if int(char) - 48 >= 0 && int(char) - 48 <= 9 {
-				ans = 10 * ans
-				ans = ans + int(char) - 48
-			} else {
-				return 0
-			}
-		}
-		return (-1)*ans
-	} else if s[0] == '+' {
-		for _, char := range s {
-			if sign {
-				sign = false
-				continue
-			}
-			if int(char) - 48 >= 0 && int(char) - 48 <= 9 {
-				ans = 10 * ans
-				ans = ans + int(char) - 48
-			} else {
-				return 0
-			}
-		}
-		return ans
+	if s == "" {
+		return 0
 	} else {
-		for _, char := range s {
-			if int(char) - 48 >= 0 && int(char) - 48 <= 9 {
-				ans = 10 * ans
-				ans = ans + int(char) - 48
-			} else {
-				return 0
-			}
+		if s[0] == '-' || s[0] == '+' {
+			sign = true;
 		}
-		return ans
+		if s[0] == '-' {
+			for _, char := range s {
+				if sign {
+					sign = false
+					continue
+				}
+				if int(char) - 48 >= 0 && int(char) - 48 <= 9 {
+					ans = 10 * ans
+					ans = ans + int(char) - 48
+				} else {
+					return 0
+				}
+			}
+			return (-1)*ans
+		} else if s[0] == '+' {
+			for _, char := range s {
+				if sign {
+					sign = false
+					continue
+				}
+				if int(char) - 48 >= 0 && int(char) - 48 <= 9 {
+					ans = 10 * ans
+					ans = ans + int(char) - 48
+				} else {
+					return 0
+				}
+			}
+			return ans
+		} else {
+			for _, char := range s {
+				if int(char) - 48 >= 0 && int(char) - 48 <= 9 {
+					ans = 10 * ans
+					ans = ans + int(char) - 48
+				} else {
+					return 0
+				}
+			}
+			return ans
+		}
 	}
-	
 }
