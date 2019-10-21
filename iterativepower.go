44df21bd5e1490087900eea28a1bfa1f59310737
Miras <sektor.miras@gmail.com>
2019-10-21 18:39:43 +0600

    powerI

---
+++ iterativepower.go
@@ -0,0 +1,15 @@
+package iterativepower
+
+func IterativePower(nb int, power int) int {
+	if power < 0 {
+		return 0
+	} else if power == 0 {
+		return 1
+	} else {
+		ans := 0
+		for i := 1; i <= power; i++ {
+			ans = ans + nb
+		}
+		return ans
+	}
+}
