be9e7991e132b672cd2063c061c20e0c37060f8b
Miras <sektor.miras@gmail.com>
2019-10-21 18:45:36 +0600

    RecPow

--- recursivepower.go
+++ recursivepower.go
@@ -6,6 +6,6 @@ func RecursivePower(nb int, power int) int {
 	} else if power == 0 {
 		return 1
 	} else {
-		return RecursivePower(nb, power-1)*nb
+		return RecursivePower(nb, power-1) * nb
 	}
 }
