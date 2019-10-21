df0176c637c6f31a813b1178812b0958f7e09d53
Miras <sektor.miras@gmail.com>
2019-10-21 19:08:25 +0600

    change

--- findnextprime.go
+++ findnextprime.go
@@ -4,7 +4,7 @@ func FindNextPrime(nb int) int {
 	if nb > 1 {
 		for i := 2; i < nb; i++ {
 			if nb%i == 0 {
-				return FindNextPrime(nb+1)
+				return FindNextPrime(nb + 1)
 			}
 		}
 		return nb
