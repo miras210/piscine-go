63099ff1732629b277d27393c1d2617d515efa5b
Miras <sektor.miras@gmail.com>
2019-10-21 19:03:33 +0600

    prime

--- isprime.go
+++ isprime.go
@@ -1,10 +1,14 @@
 package isprime
 
 func IsPrime(nb int) bool {
-	for i := 1; i*i <= nb; i++ {
-		if nb%i == 0 {
-			return true
+	if nb > 0 {
+		for i := 1; i*i <= nb; i++ {
+			if nb%i == 0 {
+				return false
+			}
 		}
+		return true
+	} else {
+		return false
 	}
-	return false
 }
