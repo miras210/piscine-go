37dbabc3bd214b8b87038ae0b2274cf45d898827
Miras <sektor.miras@gmail.com>
2019-10-21 18:29:01 +0600

    factI

--- iterativefactorial.go
+++ iterativefactorial.go
@@ -1,10 +1,12 @@
 package iterativefactorial
 
 func IterativeFactorial(nb int) int {
-	if nb == 1 {
-		return 1
-	} else if nb >= 1 {
-		return IterativeFactorial(nb-1) * nb
+	if nb >= 0 {
+		ans := 1
+		for i := 2; i <= nb; i++ {
+			ans = ans*i
+		}
+		return ans
 	} else {
 		return 0
 	}
