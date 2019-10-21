9e31b3c1c552158ee1cab52ce649f5e55aa71d3c
Miras <sektor.miras@gmail.com>
2019-10-21 18:20:55 +0600

    factI

---
+++ iterativefactorial.go
@@ -0,0 +1,11 @@
+package iterativefactorial
+
+func IterativeFactorial(nb int) int {
+	if nb == 1 {
+		return 1 
+	} else if nb >= 1 {
+		return IterativeFactorial(nb-1)*nb
+	} else {
+		return 0
+	}
+}
