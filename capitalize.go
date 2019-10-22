df390a8850b6ecbb5447a1957c04a974f656a22d
Miras <sektor.miras@gmail.com>
2019-10-22 20:50:06 +0600

    change

---
+++ capitalize.go
@@ -0,0 +1,19 @@
+package cap
+
+func Capitalize(s string) string {
+	var s1 string
+	var prev rune = 0
+	for _, char := range s {
+		if char >= 'a' && char <= 'z' {
+			if !((prev >= 'a' && prev <= 'z') || (prev >= 'A' && prev <= 'Z') || (prev >= '0' && prev <= '9')) {
+				s1 += string(char - 32)
+			} else {
+				s1 += string(char)
+			}
+		} else {
+			s1 += string(char)
+		}
+		prev = char
+	}
+	return s1
+}
