533916724b3f9c56d2fad053f37723eb644ad1ba
Miras <sektor.miras@gmail.com>
2019-10-22 20:07:20 +0600

    comp

--- compare.go
+++ compare.go
@@ -11,9 +11,9 @@ func Compare(a, b string) int {
 	}
 	for i, j := 0, 0; i < count1 && j < count2; i, j = i+1, j+1 {
 		if a[i] > b[j] {
-			return -1
-		} else if a[i] < b[j] {
 			return 1
+		} else if a[i] < b[j] {
+			return -1
 		}
 	}
 	return 0
