67853c598098a194220bc770316085b5fb611eed
Miras <sektor.miras@gmail.com>
2019-10-22 18:37:40 +0600

    2

--- printnbrinorder.go
+++ printnbrinorder.go
@@ -3,8 +3,12 @@ package order
 import "github.com/01-edu/z01"
 
 func PrintNbrInOrder(n int) {
-	s := Itoa(n)
-	var s1 string
+	var s string
+	s += rune(n % 10) + 48 
+	for n != 0 {
+		n = n / 10
+		s += rune(n % 10) + 48
+	}
 	for i := '0'; i <= '9'; i++ {
 		for _, char := range s {
 			if i == char {
