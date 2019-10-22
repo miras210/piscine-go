f2ae49059eac763f9bf3320aea232b6d07382c66
Miras <sektor.miras@gmail.com>
2019-10-22 21:37:03 +0600

    join

--- join.go
+++ join.go
@@ -2,8 +2,15 @@ package join
 
 func Join(strs []string, sep string) string {
 	var ans string
+	var count int = 0
 	for i := range strs {
-		ans += strs[i] + sep
+		count++
+	}
+	for i := range strs {
+		ans += strs[i]
+		if i != count - 1 {
+			ans += sep
+		}
 	}
 	return ans
 }
