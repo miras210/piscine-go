3e3d0b0f15370c04df3b627468dac5d4195cb36a
Miras <sektor.miras@gmail.com>
2019-10-22 21:03:29 +0600

    alpha

---
+++ isalpha.go
@@ -0,0 +1,10 @@
+package alpha
+
+func IsAlpha(str string) bool {
+	for _, char = range str {
+		if !((char >= 'a' && char <= 'z') || (char >= 'A' && char <= 'Z') || (char >= '0' && char <= '9')) {
+			return false
+		}
+	}
+	return true
+}
