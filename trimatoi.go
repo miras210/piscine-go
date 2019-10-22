ea436ac09e613cf706d1d65754e6d984ca3e91ca
Miras <sektor.miras@gmail.com>
2019-10-22 18:51:49 +0600

    atoi

---
+++ trimatoi.go
@@ -0,0 +1,20 @@
+package atoi
+
+func TrimAtoi(s string) int {
+	var sign bool = true
+	var int num
+	for _, char := range s {
+		if char >= '0' && char <= '9' {
+			num = num * 10
+			num = num + int(char) - 48
+		}
+		if char == '-' {
+			sign = false
+		}
+	}
+	if sign {
+		return num
+	} else {
+		return (-1)*num
+	} 
+}
