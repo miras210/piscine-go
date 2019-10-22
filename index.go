730cea8b734218ede2cac60a69855c4b74026351
Miras <sektor.miras@gmail.com>
2019-10-22 19:32:00 +0600

    index

---
+++ index.go
@@ -0,0 +1,23 @@
+package index
+
+func Index(s string, toFind string) int {
+	var ind int = -1
+	var count int = 0
+	for index, char := range s {
+		if char == rune(toFind[count]) {
+			ind = index + 1
+			count++ 
+		} else {
+			ind = -1
+			count = 0
+		}
+		if count >= len(toFind) {
+			break
+		}
+	}
+	if ind == -1 {
+		return ind
+	} else {
+		return ind - count
+	}
+}
