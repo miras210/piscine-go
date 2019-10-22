87e0052129688ebde4876f969548dbf54f96600a
Miras <sektor.miras@gmail.com>
2019-10-22 20:02:23 +0600

    task

---
+++ compare.go
@@ -0,0 +1,21 @@
+package compare
+
+func Compare(a, b string) int {
+	var equal int = 0
+	count1 := 0
+	count2 := 0
+	for range a {
+		count1++
+	}
+	for range b {
+		count2++
+	}
+	for i, j := 0, 0; i < count1 && j < count2; i++, j++ {
+		if a[i] > b[j] {
+			return -1
+		} else if a[i] < b[j] {
+			return 1
+		}
+	}
+	return 0
+}
