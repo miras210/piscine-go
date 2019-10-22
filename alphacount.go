e18ff8eba29431f3f102740a4d3439f2f3476795
Miras <sektor.miras@gmail.com>
2019-10-22 18:24:31 +0600

    push

--- alphacount.go
+++ alphacount.go
@@ -4,10 +4,10 @@ import "github.com/01-edu/z01"
 
 func AlphaCount(str string) int {
 	count := 0
-	for _,char := range str {
+	for _, char := range str {
 		if (char >= 'a' && char <= 'z') || (char >= 'A' && char <= 'Z') {
 			count++
-		} 
+		}
 	}
 	return count
 }
