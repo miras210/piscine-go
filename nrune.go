535cbe4a60a1097901c3ac623f6bbb1b8fd1fb1e
Miras <sektor.miras@gmail.com>
2019-10-22 19:13:36 +0600

    changes

--- nrune.go
+++ nrune.go
@@ -1,9 +1,11 @@
 package nRune
 
-func NRune(s string) rune {
+func NRune(s string, n int) rune {
 	var a rune = '0'
-	for _, char := range s {
-		a = char
+	for i, char := range s {
+		if i == n-1 {
+			a = char
+		}
 	}
 	return a
 }
