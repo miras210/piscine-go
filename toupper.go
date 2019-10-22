fbeec9188701e1362318b70daef7041a3857e266
Miras <sektor.miras@gmail.com>
2019-10-22 20:14:33 +0600

    up

--- toupper.go
+++ toupper.go
@@ -2,7 +2,7 @@ package toup
 
 func ToUpper(s string) string {
 	var s1 string
-	for _,char := range s {
+	for _, char := range s {
 		if char >= 'a' && char <= 'z' {
 			s1 += string(char - 32)
 		} else {
