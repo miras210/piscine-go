b0d6c2c1c6edad9de7ba7babacec06e92585e450
Miras <sektor.miras@gmail.com>
2019-10-23 18:41:24 +0600

    rev

---
+++ revparams/main.go
@@ -0,0 +1,18 @@
+package main
+
+import "os"
+import "github.com/01-edu/z01"
+
+func main() {
+	arg := os.Args
+	count := 0
+	for i := range arg {
+		count++
+	}
+	for i := count; i > 0; i-- {
+		for _, char := range arg[i] {
+			z01.PrintRune(char)
+		}
+		z01.PrintRune('\n')
+	}
+}
