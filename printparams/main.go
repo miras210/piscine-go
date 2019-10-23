243c5b0a7d78f6679f723c3cfb117d0282511274
Miras <sektor.miras@gmail.com>
2019-10-23 18:33:49 +0600

    params

---
+++ printparams/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "os"
+import "github.com/01-edu/z01"
+
+func main() {
+	arg := os.Args
+	for i := range arg {
+		for _,char := range arg[i] {
+			z01.PrintRune(char)
+		}
+		z01.PrintRune('\n')
+	}
+}
