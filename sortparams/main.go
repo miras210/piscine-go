2d02927286c5199bee122c863cb23a28ccb9ca86
Miras <sektor.miras@gmail.com>
2019-10-23 18:53:00 +0600

    sort

--- sortparams/main.go
+++ sortparams/main.go
@@ -5,15 +5,16 @@ import "github.com/01-edu/z01"
 
 func main() {
 	arg := os.Args
-	count := 0
-	for i, char := range arg {
-		if i != 0 {
-			for j := ' '; j <= '~'; j++ {
-				if j == char {
-					z01.PrintRune(char)
+	for j := '!'; j <= '~'; j++ {
+		for i := range arg {
+			if i != 0 {
+				for _, char := range arg[i] {
+					if j == char {
+						z01.PrintRune(char)
+						z01.PrintRune('\n')
+					}
 				}
 			}
-			z01.PrintRune('\n')
 		}
 	}
 }
