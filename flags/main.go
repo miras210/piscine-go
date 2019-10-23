b973aa0ce484ab6831fc347ab98a4225e5860963
Miras <sektor.miras@gmail.com>
2019-10-23 20:03:59 +0600

    flags

---
+++ flags/main.go
@@ -0,0 +1,98 @@
+package main
+
+import "os"
+import "github.com/01-edu/z01"
+
+func print(s string) {
+	for _, char := range s {
+		z01.PrintRune(char)
+	}
+}
+
+func Insert(add string, s string) string {
+	var Nadd string
+	beg := false
+	for _, char := range add {
+		if beg {
+			Nadd += string(char)
+		}
+		if char == '=' {
+			beg = true
+		}
+	}
+	return s + Nadd
+}
+
+func Order(s string) string {
+	var ans string
+	for i := '!'; i <= '~'; i++ {
+		for _, char := range s {
+			if i == char {
+				ans += string(char)
+			}
+		}
+	}
+	return ans
+}
+
+func Help(s string) {
+	for _, char := range s {
+		z01.PrintRune(char)
+	}
+}
+
+func main() {
+	arg := os.Args[1:]
+	var ans string
+	isFlag := false
+	isEmpty := true
+	text := `--insert
+	-i
+	  This flag inserts the string into the string passed as argument.
+  --order
+	-o
+	  This flag will behave like a boolean, if it is called it will order the argument.`
+	count := 0
+	for range arg {
+		count++
+	}
+	if count == 0 {
+		Help(text)
+	} else if count == 1 {
+		if arg[0] == "-h" || arg[0] == "--help" {
+			Help(text)
+		} else {
+			print(arg[0])
+		}
+	} else {
+		for i := range arg {
+			for _, char := range arg[i] {
+				if isFlag {
+					if char == 'i' {
+						ans = Insert(arg[i], arg[count-1])
+						isEmpty = false
+						isFlag = false
+						break
+					} else if char == 'o' {
+						if isEmpty == false {
+							ans = Order(ans)
+						} else {
+							ans = Order(arg[count-1])
+						}
+						isFlag = false
+						break
+					} else if char == 'h' {
+						Help(text)
+						isFlag = false
+						break
+					}
+				}
+				if char == '-' {
+					isFlag = true
+				}
+			}
+		}
+		print(ans)
+	}
+	z01.PrintRune('\n')
+}
