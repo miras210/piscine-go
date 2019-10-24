b7ffc0369b255404038402ca6b3aff35f8947792
Miras <sektor.miras@gmail.com>
2019-10-24 21:49:21 +0600

    wtf

--- splitwhitespaces.go
+++ splitwhitespaces.go
@@ -2,23 +2,29 @@ package split
 
 func SplitWhiteSpaces(str string) []string {
 	count := 0
+	single := true
 	for _, char := range str {
-		if char == '\n' || char == ' ' || char == '\t' {
-			count++
+		if !(char == '\n' || char == ' ' || char == '\t') {
+			single = true
+		} else {
+			if single {
+				count++
+				single = false
+			}
 		}
 	}
 	arr := make([]string, count+1)
 	i := 0
-	next := false
+	duble := true
 	for i != count {
 		for _, char := range str {
 			if !(char == '\n' || char == ' ' || char == '\t') {
 				arr[i] += string(char)
-				next = true
+				duble = true
 			} else {
-				if next {
+				if duble {
 					i++
-					next = false
+					duble = false
 				}
 			}
 		}
