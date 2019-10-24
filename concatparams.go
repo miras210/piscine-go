51ffb261b5e8f204e99de7c1b853a8f84a83b05b
Miras <sektor.miras@gmail.com>
2019-10-24 21:01:40 +0600

    params

---
+++ concatparams.go
@@ -0,0 +1,17 @@
+package concat
+
+func ConcatParams(args []string) string {
+	var ans string
+	count := 0
+	for range args {
+		count++
+	}
+	for i, str := range args {
+		ans += str
+		if i != count-1 { 
+			ans += "\n"
+		}
+	}
+	return ans
+}
+
